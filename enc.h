66caecdcb184cbd931418b6ace1104813408e3ac
Jake Clarke <clarke.jake@gmail.com>
2013-03-24 19:56:46 +0000

    Added messages.h and enc.h to makefile. Fixed casting and compile errors.

--- enc.h
+++ enc.h
@@ -3,24 +3,24 @@
 #include <sys/socket.h>
 
 
-const int MAGIC_NUMBER = 1421;
+const char MAGIC_NUMBER = 142;
 
 ssize_t esend(int sockfd, const void *buf, size_t len, int flags) {
 
 	char ebuf[len];
 
 	for(int i = 0; i < len; i++) {
-		ebuf[i] = buf[i] ^ MAGIC_NUMBER;
+		ebuf[i] = ((char *)buf)[i] ^ MAGIC_NUMBER;
 	}
 
 	return send(sockfd, &ebuf, len, flags);
 }
 
 ssize_t drecv(int sockfd, void *buf, size_t len, int flags)  {
-	ssize_t len = recv(sockfd, buf, len, flags);
+	ssize_t rlen = recv(sockfd, buf, len, flags);
 
-	for(int i = 0; i < len; i ++) {
-		buf[i] = buf[i] ^ MAGIC_NUMBER;
+	for(int i = 0; i < rlen; i ++) {
+		((char *)buf)[i] = ((char *)buf)[i] ^ MAGIC_NUMBER;
 	}
 
 	return len;
--- makefile
+++ makefile
@@ -1,10 +1,10 @@
 flags = -g
 all: aos-client aos-server
 
-aos-client: client.c
+aos-client: client.c enc.h messages.h
 	clang $(flags) -pthread -o aos-client client.c
 
-aos-server: server.c
+aos-server: server.c enc.h messages.h
 	clang $(flags) -o aos-server server.c
 
 clean:
