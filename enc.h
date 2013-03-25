f613f2f2e8d7ca31b0cbfd2b1e8372e8e4d82bbb
Jake Clarke <clarke.jake@gmail.com>
2013-03-25 02:41:44 +0000

    Commented enc.h.

--- enc.h
+++ enc.h
@@ -2,27 +2,28 @@
 #define ENC_H
 #include <sys/socket.h>
 
-#define MAGIC_NUMBER 142;
+#define MAGIC_NUMBER 142; // magic number for the xor function.
 
 ssize_t esend(int sockfd, const void *buf, size_t len, int flags) {
 
 	char ebuf[len]; // temp buffer, don't want to edit our source buffer.
 
 	for(int i = 0; i < len; i++) {
-		ebuf[i] = ((char *)buf)[i] ^ MAGIC_NUMBER;
+		ebuf[i] = ((char *)buf)[i] ^ MAGIC_NUMBER; // xor with the magic number to get our encrypted form.
 	}
 
-	return send(sockfd, &ebuf, len, flags);
+	return send(sockfd, &ebuf, len, flags); // send.
 }
 
 ssize_t drecv(int sockfd, void *buf, size_t len, int flags)  {
-	ssize_t rlen = recv(sockfd, buf, len, flags);
+	ssize_t rlen = recv(sockfd, buf, len, flags); 
 
+	// not using a temp buffer because we can just modify data in place.
 	for(int i = 0; i < rlen; i ++) {
-		((char *)buf)[i] = ((char *)buf)[i] ^ MAGIC_NUMBER;
+		((char *)buf)[i] = ((char *)buf)[i] ^ MAGIC_NUMBER; // xor with the magic number get get our plain text form.
 	}
 
-	return rlen;
+	return rlen; // returrn the length of data recieved.
 }
 
 #endif
